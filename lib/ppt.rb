942cbf0784e000b85366ff2b705963bff6502109
EliezerCruz <eliezer1985@hotmail.com>
2012-10-19 13:35:43 +0100

    Creacion de carpetas

---
+++ lib/ppt.rb
@@ -0,0 +1,11 @@
+#
+
+class PiedraPapelTijerasGame
+	attr_reader 
+	attr_accessor :piedra, :papel , :tijeras
+	def initialize()
+          array = :piedra, :papel , :tijeras
+		@ganadoras
+		@tiradas
+
+	
